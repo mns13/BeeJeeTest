bff7d6c67a2d179230e60c2d37b2396739694d9a
Murad Nurmukhamedov <mns130691@gmail.com>
2020-08-10 22:01:41 +0300

    change config.php and .htaccess

--- test/app/config/config.php
+++ test/app/config/config.php
@@ -1,14 +1,14 @@
 <?php
   // DB params
   define ('DBHOST', 'localhost');
-  define ('DBUSER', 'root');
-  define ('DBPASS', '');
-  define ('DBNAME', 'test');
+  define ('DBUSER', '_YOUR_USER_');
+  define ('DBPASS', '_YOUR_PASS_');
+  define ('DBNAME', '_YOUR_DB_NAME_');
 
   // App root
   define('APPROOT', dirname(dirname(__FILE__))) ;
   //URL root
-  define('URLROOT', 'http://localhost/test');
+  define('URLROOT', '_YOUR_URL_');
   // Site name
   define('SITENAME', 'BeeJee Test');
 
